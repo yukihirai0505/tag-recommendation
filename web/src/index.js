06b52e693c6441d216213596cd3376f125d0f33f
Yuki Hirai <yukihirai0505@gmail.com>
2017-11-10 19:51:15 +0900

    Remove HashRouter

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history';
 
 // Containers
@@ -9,9 +9,9 @@ import Full from './containers/Full/'
 const history = createBrowserHistory();
 
 ReactDOM.render((
-  <HashRouter history={history}>
+  <Router history={history}>
     <Switch>
       <Route path="/" name="Home" component={Full}/>
     </Switch>
-  </HashRouter>
+  </Router>
 ), document.getElementById('root'));
